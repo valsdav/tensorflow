ae3e6f64eaf004ddfcbe596f3a82a70a274ddbfe
Mircho Rodozov <mrodozov@cern.ch>
2019-02-11 18:00:17 +0100

    Adjust URLs c flags etc.

--- configure.py
+++ configure.py
@@ -1557,6 +1557,7 @@ def main():
   check_bazel_version('0.19.0', '0.21.0')
 
   reset_tf_configure_bazelrc()
+  write_to_bazelrc('import %workspace%/tools/bazel.rc')
 
   cleanup_makefile()
   setup_python(environ_cp)
